c4a95cc61a69b300073627552b37327a6d148afe
iqpascal <iqpascal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-09 13:09:47 +0000

    Updated documentation
    git-svn-id: https://plugins.svn.wordpress.org/iq-block-country/tags/1.0.4@287259 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/iq-block-country.php
@@ -0,0 +1,291 @@
+<?php
+/*
+Plugin Name: iQ Block Country
+Plugin URI: http://www.trinyx.nl/2010/03/iq-block-country-a-wordpress-plugin/
+Version: 1.0.4
+Author: Pascal
+Author URI: http://www.trinyx.nl/
+Description: Block out the bad guys based on from which country the ip address is from. This plugin uses the GeoLite data created by MaxMind for the ip-to-country lookups.
+Author URI: http://www.trinyx.nl/
+License: GPL2
+*/
+
+/* This script uses GeoLite Country from MaxMind (http://www.maxmind.com) which is available under terms of GPL/LGPL */
+
+/*  Copyright 2010  Pascal  (email : pascal@trinyx.nl)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*
+ * 
+ * This software is dedicated to my one true love.
+ * Luvya :)
+ * 
+ */
+
+function iq_is_valid_ipv4($ipv4) {
+
+	if(filter_var($ipv4, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) === FALSE) {
+		return false;
+	}
+
+	return true;
+}
+
+function iq_is_valid_ipv6($ipv6) {
+
+	if(filter_var($ipv6, FILTER_VALIDATE_IP,FILTER_FLAG_IPV6) === FALSE) {
+		return false;
+	}
+
+	return true;
+}
+
+function iq_this_plugin_first() {
+	// ensure path to this file is via main wp plugin path
+	$wp_path_to_this_file = preg_replace('/(.*)plugins\/(.*)$/', WP_PLUGIN_DIR."/$2", __FILE__);
+	$this_plugin = plugin_basename(trim($wp_path_to_this_file));
+	$active_plugins = get_option('active_plugins');
+	$this_plugin_key = array_search($this_plugin, $active_plugins);
+	if ($this_plugin_key) { // if it's 0 it's the first plugin already, no need to continue
+		array_splice($active_plugins, $this_plugin_key, 1);
+		array_unshift($active_plugins, $this_plugin);
+		update_option('active_plugins', $active_plugins);
+	}
+}
+
+	
+/*
+ * Admin menu stuff
+ */
+function iqblockcountry_create_menu() {
+	//create new menu option in the settings department
+	add_submenu_page ( 'options-general.php', 'iQ Block Country', 'iQ Block Country', 'administrator', __FILE__, 'iqblockcountry_settings_page' );
+	//call register settings function
+	add_action ( 'admin_init', 'iqblockcountry_register_mysettings' );
+}
+
+function iqblockcountry_register_mysettings() {
+	//register our settings
+	register_setting ( 'iqblockcountry-settings-group', 'blockcountry_banlist' );
+	register_setting ( 'iqblockcountry-settings-group', 'blockcountry_blacklist' );
+	register_setting ( 'iqblockcountry-settings-group', 'blockcountry_whitelist' );
+}
+
+function iqblockcountry_downloadgeodatabase() {
+/*
+ * Download the GeoIP database from MaxMind
+ */
+		/* GeoLite URL */
+		$url = 'http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz';
+		$request = new WP_Http ();
+		$result = $request->request ( $url );
+		$content = array ();
+		
+		if ((isset ( $result->errors )) || (! (in_array ( '200', $result ['response'] )))) {
+			print "<p>Error occured: Could not download the GeoIP database from $url.<br />";
+			print "Please download this file yourself and unzip this file to $geodbfile</p>";
+		} else {
+
+			global $geodbfile;
+			
+			/* Download file */
+			if (file_exists ( $geodbfile . ".gz" )) { unlink ( $geodbfile . ".gz" ); }
+			$content = $result ['body'];
+			$fp = fopen ( $geodbfile . ".gz", "w" );
+			fwrite ( $fp, "$content" );
+			fclose ( $fp );
+			
+			/* Unzip this file and throw it away afterwards*/
+			$zd = gzopen ( $geodbfile . ".gz", "r" );
+			$buffer = gzread ( $zd, 2000000 );
+			gzclose ( $zd );
+			if (file_exists ( $geodbfile . ".gz" )) { unlink ( $geodbfile . ".gz" ); }
+			
+			/* Write this file to the GeoIP database file */
+			if (file_exists ( $geodbfile )) { unlink ( $geodbfile ); } 
+			$fp = fopen ( $geodbfile, "w" );
+			fwrite ( $fp, "$buffer" );
+			fclose ( $fp );
+			print "<p>Finished downloading</p>";
+		}
+}
+
+function iqblockcountry_settings_page() {
+	?>
+<div class="wrap">
+<h2>iQ Block Countries</h2>
+
+		<strong>The GeoIP database is updated once a month. Be sure to update this file every now and then by clicking the following button:</strong>
+        
+		<form name="download_geoip" action="#download" method="post">
+        <input type="hidden" name="action" value="download" />
+<?php 
+        echo '<div class="submit"><input type="submit" name="test" value="' . __( 'Download new GeoIP Database', 'iq-block-country' ) . '" /></div>';
+        wp_nonce_field('iq-block-country');
+        echo '</form>';
+		
+        if ( $_POST[ 'action' ] == 'download') {
+			echo "Downloading....";	
+			iqblockcountry_downloadgeodatabase();	
+		}
+        
+        
+        ?>
+
+<form method="post" action="options.php">
+    <?php
+	settings_fields ( 'iqblockcountry-settings-group' );
+    if (!class_exists(GeoIP))
+	{
+		include_once("geoip.inc");
+	}
+	if (class_exists(GeoIP))
+	{
+		
+		/* Create an array with all countries that the database knows */
+		$geo = new GeoIP ();
+		$countrycodes = $geo->GEOIP_COUNTRY_CODE_TO_NUMBER;
+		$countries = $geo->GEOIP_COUNTRY_NAMES;
+		$countrylist = array ();
+		foreach ( $countrycodes as $key => $value ) {
+			if (!empty($value))
+			{
+			$countrylist [$key] = $countries [$value];
+			}
+		}
+		/* Display this array on the admin page and select any country that was already set */
+		?>
+    	    <table class="form-table">
+			<tr valign="top">
+				<th scope="row">Countries to block:<br />
+				Use the ctrl key to select multiple countries</th>
+				<td><select name="blockcountry_banlist[]" multiple="multiple" style="height: 450px;">
+    	        <?
+			$haystack = get_option ( 'blockcountry_banlist' );
+			foreach ( $countrylist as $key => $value ) {
+			print "<option value=\"$key\"";
+			if (is_array($haystack) && in_array ( $key, $haystack )) {
+				print " selected=\"selected\" ";
+			}
+			print ">$value</option>";
+		}
+		
+		?>
+    	        </select></td>
+			</tr>
+	
+			</table>	
+	
+			<p class="submit"><input type="submit" class="button-primary"
+				value="<?php _e ( 'Save Changes' )?>" /></p>
+	
+	<?php 
+	} 
+	else
+	{
+		print "<p>You are missing the GeoIP class. Perhaps geoip.inc is missing?</p>";	
+	
+	}
+	?>	
+
+	<p>This product includes GeoLite data created by MaxMind, available from
+	<a href="http://www.maxmind.com/">http://www.maxmind.com/</a>.</p>
+
+	<p>If you like this plugin please link back to <a href="http://www.trinyx.nl/">Trinyx.nl</a>! :-)</p>
+
+    <?php
+	global $geodbfile;
+	
+	/* Check if the Geo Database exists otherwise try to download it */
+	if (! (file_exists ( $geodbfile ))) {
+		?> 
+		<p>GeoIP database does not exists. Trying to download it...</p>
+		<?php
+		
+			iqblockcountry_downloadgeodatabase();	
+		}
+	
+	?>
+
+
+
+</form>
+</div>
+<?php
+}
+
+/*
+  * Country check stuff that happens at the frontpage
+ */
+
+function iqblockcountry_CheckCountry() {
+	if (!class_exists(GeoIP))
+	{
+		include_once("geoip.inc");
+	}
+	global $geodbfile;
+	
+	if ((file_exists ( $geodbfile )) && function_exists(geoip_open)) {
+
+		if (empty($_SERVER["HTTP_X_FORWARDED_FOR"])) {
+			$ip_address = $_SERVER["REMOTE_ADDR"];
+		} else {
+			$ip_address = $_SERVER["HTTP_X_FORWARDED_FOR"];
+		}
+
+		// First steps into being IPv6 compatible.
+		
+		$ipv4 = FALSE;
+		$ipv6 = FALSE;
+		if (iq_is_valid_ipv4($ip_address)) { $ipv4 = TRUE; }
+		if (iq_is_valid_ipv6($ip_address)) { $ipv6 = TRUE; }
+		
+		if ($ipv4) 
+		{ 	
+			$gi = geoip_open ( $geodbfile, GEOIP_STANDARD );
+			$country = geoip_country_code_by_addr ( $gi, $ip_address );
+			geoip_close ( $gi );
+		}
+		elseif ($ipv6)
+		{
+			// Do nothing at all as we're not IPv6 compatible just yet.
+			$country = 'ipv6';
+		}
+		
+		$badcountries = get_option( 'blockcountry_banlist' );
+
+		/* Check if we have one of those bad guys */
+		if (is_array ( $badcountries ) && in_array ( $country, $badcountries )) {
+			header ( 'HTTP/1.1 403 Forbidden' );
+			print "<p><strong>Forbidden - Users from your country are not permitted to browse this site.<strong></p>";
+			
+			exit ();
+		}
+	
+	}
+
+}
+
+/*
+ * Main things
+ */
+$geodbfile = WP_PLUGIN_DIR . "/" . dirname ( plugin_basename ( __FILE__ ) ) . "/GeoIP.dat";
+
+add_action ( "activated_plugin", "iq_this_plugin_first");
+add_action ( 'wp_head', 'iqblockcountry_checkCountry', 1 );
+add_action ( 'admin_menu', 'iqblockcountry_create_menu' );
+
+?>
---
+++ trunk/readme.txt
@@ -0,0 +1,117 @@
+=== iQ Block Country ===
+Contributors: iqpascal
+Donate link: http://www.unicef.org/
+Tags: spam, block, countries, country, comments, ban
+Requires at least: 2.9.2
+Tested up to: 3.0.1
+Stable tag: 1.0.4
+
+Block out the bad guys based on from which country the ip address is from.
+
+== Description ==
+
+If you want to block people from certain (obscure) countries that have no business visiting your
+blog and perhaps only leave spam comments or other harmful actions than this is the plugin for you.
+
+Choose which countries you want to ban from visiting your blog. Any visitors from that country get 
+a HTTP/403 error with the message "Forbidden - Users from your country are not permitted to browse 
+this site."
+
+They will not be able to do harmful things to your blog like post comment spam.
+
+This plugin uses the GeoLite database from Maxmind. It has a 99.5% accuracy so that is pretty good.
+
+Once you setup this plugin it will try to download the GeoIP database from Maxmind so you will 
+always have a recent version of the database when installing this plugin.
+
+== Installation ==
+
+1. Unzip the archive and put the `iq-block-country` folder into your plugins folder (/wp-content/plugins/).
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to the settings page and choose which countries you want to ban. Use the ctrl key to select
+multiple countries
+4. Check if it downloaded the GeoIP database from MaxMind otherwise follow instructions on screen.
+
+== Frequently Asked Questions ==
+
+= Does this plugin also work with IPv6? =
+
+No not yet. Maxmind has a GeoIPv6 database though just no support for PHP yet. 
+And as my blog is also reachable on IPv6 I might incoporate this database as soon this is possible.
+
+However: We need more IPv6 out there so please DO ask your hosting provider for IPv6!
+
+= How can I get a new version of the GeoIP database? =
+
+Since version 1.0.4 you can press the "Download new GeoIP database" from the admin page
+to download a new version of the database. You do not need to download it more than
+once a month since the lite database is only updated once a month.
+
+You can also remove the file GeoIP.dat from the plugin directory and after removal go to
+the settings page of this plugin. When it sees the GeoIP database is missing it will
+try to download it for you.
+
+However you can also download the GeoIP database yourself from Maxmind and overwrite
+the existing database.
+
+Maxmind updates the GeoLite database every month.
+
+= Help it gives some error about not being able to download the GeoIP database? =
+
+Follow the instructions on screen. It will probably tell you that you have to manually
+download the GeoIP database from Maxmind from the following url:
+
+http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
+
+It will also give you the location it expects the GeoIP.dat file. So go ahead and
+download it and unzip the file to this specific location.
+
+= How come that I still see visitors from countries that I blocked in Statpress or other statistics software? =
+
+It’s true that you might see hits from countries that you have blocked in your statistics software. 
+That is the way it works, certain plugins may be run before iQ Block Country is run so it may log visitors to pages. 
+This however does not mean this plugin does not work, it just means somebody tried to access a certain page or pages 
+and that that fact is logged.
+
+If you are worried this plugin does not work you could try to block your own country and afterwards visit your 
+frontend website and see if it actually works. Also if you have access to the logfiles of the webserver that hosts 
+your website  you can see that these visitors are actually denied with a HTTP error 403.
+
+== Changelog ==
+
+= 1.0.4 =
+* Added a button to download the new GeoIP database.
+
+= 1.0.3 =
+* FAQ updated
+* Try to make sure this plugin is loaded first to avoid "headers already sent" trouble.
+
+= 1.0.2 =
+* PHP 5.2 or higher required
+* Fixed an include bug when other plugins also use the MaxMind database. Thanks to Marcus from LunaWorX for finding this bug.
+
+= 1.0.1 =
+* Included the necessary geoip.inc file.. *duh*
+
+= 1.0 =
+* Initial release
+
+== Upgrade Notice ==
+
+= 1.0.4 = 
+
+None, this is just a minor update.
+
+= 1.0.3 = 
+
+This baby should just upgrade fine. You may want to deactivate and reactivate this plugin to make (pretty) sure this
+plugin is loaded first. This especially recommended if you have problems with 'headers already sent' notices.
+
+= 1.0.2 =
+PHP 5.2 or higher is now required.
+
+Just upgrade if you have PHP 5.2 or higher.
+
+= 1.0.1 =
+Critical upgrade for this plugin to work as it was missing a necessary file.
+
